d614cef97bd0b9d8534e626acc23642e626fc760
ruihuang Ding <65103494+drh746@users.noreply.github.com>
2026-07-29 23:39:07 +0100

    Add: binary-tree-right-side-view - submission-3

---
+++ Data Structures & Algorithms/binary-tree-right-side-view/submission-3.py
@@ -0,0 +1,58 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+    def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
+        # BFS:
+        # if not root:
+        #     return []
+        # q = deque([root])
+        # res = []
+        # while q:
+        #     n = len(q)
+        #     for i in range(n):
+        #         node = q.popleft()
+        #         if i == n - 1:
+        #             res.append(node.val)
+        #         if node.left:
+        #             q.append(node.left)
+        #         if node.right:
+        #             q.append(node.right)
+        # return res
+
+        # dfs
+        # res = []
+        # def dfs(node, depth):
+        #     if not node:
+        #         return
+        #     if len(res) == depth:
+        #         res.append(node.val)
+        #     dfs(node.right, depth + 1)
+        #     dfs(node.left, depth + 1)
+        # dfs(root, 0)
+        # return res
+
+        # iterative
+        res = []
+        stack = [(root, 0)]
+        while stack:
+            node, depth = stack.pop()
+            if not node:
+                continue
+            if depth == len(res):
+                res.append(node.val)
+            if node.left:
+                stack.append((node.left, depth+1))
+            if node.right:
+                stack.append((node.right, depth+1))
+        return res
+
+
+
+            
+
+
